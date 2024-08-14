6c68540c6af2c6cb0a3b04b27b251bc9058cf8bf
Ayush Rawat <97330088+ayush156rawat@users.noreply.github.com>
2024-08-14 12:55:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import BookingsPage from "./pages/BookingsPage.jsx";
 import BookingPage from "./pages/BookingPage.jsx";
 
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://backend-1llhj6o67-ayush-rawats-projects-bfa73bf3.vercel.app/';
 axios.defaults.withCredentials = true;
 
 function App() {
